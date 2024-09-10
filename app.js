05d722182b4d3c68ceac622ae305784e36220e2f
markvilly <markpaulbusiness66@gmail.com>
2024-09-10 16:37:19 +0300

    Implemented the Current Time presentation

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const volumeSlider = document.getElementById("volume-slider");
 const muteIconContainer = document.getElementById("mute-icon");
 
 let muteState = "unmute";
-
 let playState = "play";
 
 //playAnimation
@@ -70,4 +69,56 @@ seekSlider.addEventListener("input", (e) => {
 });
 volumeSlider.addEventListener("input", (e) => {
   showRangeProgress(e.target);
+});
+
+/** Implementation of the functionality of the audio player */
+
+const audio = document.querySelector("audio");
+const durationContainer = document.getElementById("duration");
+
+const calculateTime = (secs) => {
+  const minutes = Math.floor(secs / 60);
+  const seconds = Math.floor(secs % 60);
+  const returnedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
+  return `${minutes}:${returnedSeconds}`;
+};
+
+const displayDuration = () => {
+  durationContainer.textContent = calculateTime(audio.duration);
+};
+
+const setSliderMax = () => {
+  seekSlider.max = Math.floor(audio.duration);
+};
+
+const displayBufferedAmount = () => {
+  const bufferedAmount = Math.floor(
+    audio.buffered.end(audio.buffered.length - 1)
+  );
+  audioPlayerContainer.style.setProperty(
+    "--buffered-width",
+    `${(bufferedAmount / seekSlider.max) * 100}%`
+  );
+};
+
+if (audio.readyState > 0) {
+  displayDuration();
+  setSliderMax();
+  displayBufferedAmount();
+} else {
+  audio.addEventListener("loadedmetadata", () => {
+    displayDuration();
+    setSliderMax();
+    displayBufferedAmount();
+  });
+}
+
+audio.addEventListener("progress", displayBufferedAmount);
+
+//TO CAPTURE THE CURRENT TIME VALUE.
+
+const currentTimeContainer = document.getElementById("current-time");
+
+seekSlider.addEventListener("input", () => {
+  currentTimeContainer.textContent = calculateTime(seekSlider.value);
 });
